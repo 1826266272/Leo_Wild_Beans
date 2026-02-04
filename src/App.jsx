bac744dcc1263f37c9b1aa0f570362d162c17759
Sethu <sethusince2004@gmail.com>
2026-02-04 14:35:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { scrolltrigger, splitText } from 'gsap/all';
+
+gsap.register
+
+const App = () => {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
