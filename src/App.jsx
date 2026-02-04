9a5e4a7e59e3d21107f8932e9be7272c18b76c6d
Sethu <sethusince2004@gmail.com>
2026-02-04 18:56:21 +0530

    implemented hero-section with coffee gsap animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { scrolltrigger, splitText } from 'gsap/all';
+import React from "react";
+import { gsap } from "gsap";
+import scrollTrigger from "gsap/ScrollTrigger";
+import splitText from "gsap/SplitText";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
-gsap.register
+gsap.registerPlugin(scrollTrigger, splitText);
 
 const App = () => {
 
   return (
     <>
-      
+      <Navbar/>
+      <Hero/>
+      <div className="h-dvh bg-black"></div>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import gsap from "gsap";
+import splitText from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
+import MediaQuery, { useMediaQuery } from "react-responsive";
+
+
+const Hero = () => {
+    const videoRef = React.useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+
+    useGSAP(() => {
+        const heroSplit = new splitText(".title", {
+            type: "words, chars",
+        });
+        const paragraphSplit = new splitText(".subtitles", {
+            type: "lines",
+        });
+
+        heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+
+        gsap.from(heroSplit.chars, {    
+            yPercent: 100,
+            duration: 1.3,
+            stagger: 0.09,
+            ease: "expo.out",
+        });
+
+        gsap.from(paragraphSplit.lines, {
+            yPercent: 100,
+            opacity: 0,
+            duration: 1.8,
+            ease: "expo.out",
+            delay: 1,
+            stagger: 0.06,
+
+        })
+
+        const startValue = isMobile ? "top 50%" : "center 60%";
+        const endValue = isMobile ? "60% top" : "bottom top";
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration/2,
+            })
+        }
+
+    }, []);
+
+    return (
+    <>
+        <section id="hero" className="noisy">
+            <h1 className="title">WILD BEANS</h1>
+
+            <div className="body">
+                <div className="content">
+                    <div className="space-y-5 hidden lg:block">
+                        <p>Brewed by</p>
+                        <p className="subtitles">
+                           Classic Traditon
+                        </p>
+                    </div>
+
+                    <div className="view-brews">
+                        <p className="subtitles">
+                            "I am a humble tea merchant, pouring out the elixir of life to the world."<br/> - Kakuzo Okakura.
+                        </p>
+                        <a href="#Brews">Our Brews</a>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+        <div className="video absolute inset-0">
+            <video 
+            ref={videoRef}
+            src="/videos/output.mp4" 
+            muted 
+            playsInline 
+            preload="auto"></video>
+        </div>
+    </>
+  )
+}
+
+export default Hero;
