1e9885b2a5155154df05f9d74dac17d8584d1c4e
Sethu <sethusince2004@gmail.com>
2026-03-24 00:58:52 +0530

    integrated Lenis for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/all";
+import Lenis from "lenis";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Brews from "./components/Brews";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import InstagramCard from "./components/InstagramCard";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -34,6 +36,22 @@ const imageArray = [
 
 
 const App = () => {
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    lenis.on("scroll", ScrollTrigger.update);
+    
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove(lenis.raf);
+    };
+  }, []);
 
   return (
     <>
